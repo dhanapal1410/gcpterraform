67dabb56036d5dbcd0fa29a6deae321361ec7b80
Dhanapal Devaraju <dhanapal.devaraju@atyeti.com>
2024-12-10 16:13:28 +0530

    bucket

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "google_storage_bucket" "bucket" {
-  name     = "my-first-tf-bucket"
-  location = "US"
-}
-
 resource "google_project_service" "storage_api" {
   project = "githubactions-444207"
   service = "storage.googleapis.com"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "my-first-tf-bucket"
+  location = "US"
 }
